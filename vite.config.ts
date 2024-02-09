cc30da8b2c30048ad74d708b7b9b8d7cfa9ebbb1
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-02-10 02:06:02 +0800

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import * as SFCCompiler from 'vue/compiler-sfc'
 
 export default defineConfig({
   plugins: [
     Vue({
       template: {
-        // @ts-expect-error
         vapor: true,
       },
-      compiler: SFCCompiler,
     }),
   ],
 })
